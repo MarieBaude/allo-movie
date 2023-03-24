75495f98515ace9066a02642091402007a1df6f5
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-03-24 14:29:24 +0100

    change header css

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <section class="mx-auto max-w-2xl sm:py-24">
+      <div class="text-center">
+        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Allo Movie
+        </h1>
+        <p class="mt-6 text-lg leading-8 text-gray-600">
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
+          aliqua.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
--- src/container/Main.jsx
+++ src/container/Main.jsx
@@ -1,20 +1,10 @@
 import React from "react";
+import Header from "../components/Header";
 
 function Main() {
   return (
     <main>
-      <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div class="text-center">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Allo Movie
-          </h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
-          </p>
-        </div>
-      </div>
+      <Header />
     </main>
   );
 }
