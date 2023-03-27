b573db81480981a665b2cdc8cf13999c6201e467
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-03-27 17:04:47 +0200

    adding search page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Main from "./container/Main"
-import Favorites from './container/Favorites';
+import Main from "./container/Main";
+import Favorites from "./container/Favorites";
 import MovieDetail from "./container/MovieDetail";
 
 function App() {
   return (
     <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/favorites" element={<Favorites/>} />
-        <Route path="/movies/:movieId" element={<MovieDetail />} />
+      <Route path="/" element={<Main />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/movies/:movieId" element={<MovieDetail />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
