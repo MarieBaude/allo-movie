929076a308a22f46e7e573ee152f04c3b418bb5b
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-03-27 14:48:12 +0200

    adding poster img

--- src/container/MovieDetail.jsx
+++ src/container/MovieDetail.jsx
@@ -22,10 +22,11 @@ export default function MovieDetail() {
       .then((data) => setMovie(data));
   }, [movieId]);
 
+  const posterPath = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+
   return (
     <Layout>
       <div className="relative isolate overflow-hidden bg-gray-600 px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
-        
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
@@ -38,11 +39,12 @@ export default function MovieDetail() {
                 </p>
               </div>
             </div>
+            <div>
+            <img src={posterPath} alt={movie.title} />
+          </div>
           </div>
 
-          <div>
-            <img src={movie.poster} alt={movie.title} />
-          </div>
+          
 
         </div>
       </div>
