4dcfd7a34c0f76dbd0f57141c4dbbd6cede49b56
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-03-27 09:59:35 +0200

    adding fetch

--- src/container/Main.jsx
+++ src/container/Main.jsx
@@ -1,34 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import Header from "../components/Header";
-import Movie from '../components/Movie';
+import Movie from "../components/Movie";
 
 function Main() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const apiKey = process.env.API;
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=fr-FR&page=1`
+    )
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results));
+  }, []);
+
   return (
     <main className="bg-gray-600">
       <Header />
 
-    <div className="flex flex-wrap justify-center">
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-    </div>
-
+      <div className="flex flex-wrap justify-center">
+        {movies.map((movie) => (
+          <Movie />
+        ))}
+      </div>
     </main>
   );
 }
